f733c9171a2349d6afdb71d3897eb9e99a22b619
Jong <104367020+Eric-JongYoung@users.noreply.github.com>
2022-07-02 22:20:33 +0900

    jquery 이용한 js 수정

--- color.js
+++ color.js
@@ -1,21 +1,23 @@
-
     var links = {
         setColor:function(color){
-            var alist = document.querySelectorAll('a');
-            var i = 0;
-            while(i < alist.length){
-                alist[i].style.color = color;
-                i = i + 1;
-                }
+            // var alist = document.querySelectorAll('a');
+            // var i = 0;
+            // while(i < alist.length){
+            //     alist[i].style.color = color;
+            //     i = i + 1;
+            //     }
+            $('a').css('color', color);
             }
         }
     var body = {
         setColor:function(color){
-            document.querySelector('body').style.color = color;
+            //document.querySelector('body').style.color = color;
+            $('body').css('color', color);
             },
         
         setBackgroundColor:function(color){
-            document.querySelector('body').style.backgroundColor = color;
+            //document.querySelector('body').style.backgroundColor = color;
+            $('body').css('backgroundColor', color);
             }
         }
     function nightDayHandler(self){
